7cd69054f32823354a7f342c9351b2df30914010
Mayank Poddar <mayankpoddar@Mayanks-MacBook-Pro-2.local>
2022-03-21 15:03:08 -0400

    Optimizer changed to Adadelta. Model Hyperparameters tuned.

--- project1_model.py
+++ project1_model.py
@@ -31,13 +31,13 @@ class BasicBlock(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_layers, num_blocks, num_classes=10):
+    def __init__(self, block, in_planes, num_layers, num_blocks, num_classes=10):
         if not isinstance(num_blocks, list):
             raise Exception("num_blocks parameter should be a list of integer values")
         if num_layers != len(num_blocks):
             raise Exception("Residual layers should be equal to the length of num_blocks list")
         super(ResNet, self).__init__()
-        self.in_planes = 64
+        self.in_planes = in_planes
         self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=3,
                                stride=1, padding=1, bias=True)
         self.bn1 = nn.BatchNorm2d(self.in_planes)
@@ -45,8 +45,8 @@ class ResNet(nn.Module):
         self.layer1 = self._make_layer(block, self.in_planes, num_blocks[0], stride=1)
         for i in range(2, num_layers+1):
             setattr(self, "layer"+str(i), self._make_layer(block, 2*self.in_planes, num_blocks[i-1], stride=2))
-        #finalshape = list(getattr(self, "layer"+str(num_layers))[-1].modules())[-2].num_features
-        self.linear = nn.Linear(1024, num_classes)
+        finalshape = list(getattr(self, "layer"+str(num_layers))[-1].modules())[-2].num_features
+        self.linear = nn.Linear(finalshape, num_classes)
         self.path = "./project1_model.pt"
 
     def _make_layer(self, block, planes, num_blocks, stride):
@@ -72,5 +72,9 @@ class ResNet(nn.Module):
     def loadFromDisk(self):
         self.load_state_dict(torch.load(self.path))
 
-def project1_model():
-    return ResNet(BasicBlock, 5, [2, 2, 2, 2, 2])
+if __name__ == "__main__":
+    model = ResNet(BasicBlock, 42, 4, [2, 2, 2, 2])
+    trainable_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(trainable_parameters)
+    x = torch.rand(1, 3, 32, 32)
+    model(x)
